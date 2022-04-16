58cf79f1782db513678811e3dd94b6c4798adcee
Norah.Waswala <norah.waswala@student.moringa.com>
2022-04-16 07:31:28 +0300

    feat:component
    add component user and repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './component/user/user.component';
+import { RepositoryComponent } from './component/repository/repository.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    RepositoryComponent
   ],
   imports: [
     BrowserModule,
