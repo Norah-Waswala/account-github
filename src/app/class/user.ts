bfe31309ae9c9d433c7fa59f039ef6ff6979d73f
Norah.Waswala <norah.waswala@student.moringa.com>
2022-04-18 16:10:49 +0300

    feat:class user

--- src/app/class/user.ts
+++ src/app/class/user.ts
@@ -1,3 +1,11 @@
 export class User {
-    username!:string;
+public login!:string;
+ public name!:string;
+  static data: any[] | undefined;
+   
+
+    
+    
+    // private clientid='f187c11ec16efb78c39e';
+    // private clientsecret='09f9e2b4b018747da61b4048d93b4abd78717cbc'
 }
